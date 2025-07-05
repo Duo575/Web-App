0f7ab8d68860d59b3c8d0c81cf8a97ed07da8aa9
SilverPhoenix57575 <130338929+SilverPhoenix57575@users.noreply.github.com>
2025-07-05 12:52:55 +0530

    refactor: restructure CompareDemo component layout for improved readability

--- src/Components/UI/CompareDemo.tsx
+++ src/Components/UI/CompareDemo.tsx
@@ -3,15 +3,17 @@ import { Compare } from "@/Components/UI/Compare";
 
 export default function CompareDemo() {
   return (
-    <div className="p-6 rounded-3xl backdrop-blur-lg bg-transparent border border-white/5 shadow-2xl shadow-black/30 hover:border-white/10 hover:shadow-3xl transition-all duration-300">
-      <Compare
-        firstImage="https://assets.aceternity.com/code-problem.png"
-        secondImage="https://assets.aceternity.com/code-solution.png"
-        firstImageClassName="object-cover object-left-top"
-        secondImageClassname="object-cover object-left-top"
-        className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
-        slideMode="hover"
-      />
+    <div className="p-6 rounded-3xl backdrop-blur-lg bg-transparent shadow-2xl shadow-black/30 hover:shadow-3xl transition-all duration-300">
+      <div className="ml-[7.5%]">
+        <Compare
+          firstImage="https://assets.aceternity.com/code-problem.png"
+          secondImage="https://assets.aceternity.com/code-solution.png"
+          firstImageClassName="object-cover object-left-top"
+          secondImageClassname="object-cover object-left-top"
+          className="h-[250px] w-[240px] md:h-[500px] md:w-[600px]"
+          slideMode="hover"
+        />
+      </div>
     </div>
   );
 }
